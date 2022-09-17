ffb3646ae7ddca373024e8055cb293d6df926028
TriLe1122 <letrin93@gmail.com>
2022-09-16 21:18:50 -0600

    second

---
+++ app/Models/Todo.js
@@ -0,0 +1,32 @@
+
+
+
+export class Todo {
+  constructor(data) {
+    this.id = data.id
+    this.description = data.description
+    this.user = data.user
+    this.completed = data.completed || false
+  }
+
+
+
+  get TodoTemplate() {
+    return /*html*/` 
+    <div class="d-flex justify-content-between mb-1 pt-2">
+              <input onchange="app.ToDosController.toggleToDo('${this.id}')" type="checkbox" name="completed" id="" ${this.completed
+        ? 'checked' : ''}>
+              <p class="border-bottom border-2 border-dark  px-2 text-center">${this.description}
+              </p>
+              <div class="d-flex ">
+                <i onclick="app.ToDosController.removeToDo('${this.id}')"
+                  class="mdi mdi-delete-forever-outline  selectable"></i>
+              </div>
+            </div>
+    
+    
+    
+    
+    `
+  }
+}
